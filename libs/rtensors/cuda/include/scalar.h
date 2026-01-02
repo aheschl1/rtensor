4b7b40ecf19ba3c4cd4a500ef1b5d51c0129f9a8
root <neithercouldterry@gmail.com>
2026-01-01 18:22:29 -0700

    added abs unary op for gpu

--- libs/rtensors/cuda/include/scalar.h
+++ libs/rtensors/cuda/include/scalar.h
@@ -123,6 +123,19 @@ void launch_tanh_strided_f64(double* data, size_t offset, ptrdiff_t stride, size
 void launch_tanh_nd_affine_f32(float* data, size_t offset, const ptrdiff_t* stride, const size_t* shape, size_t rank, size_t size, unsigned int block_size);
 void launch_tanh_nd_affine_f64(double* data, size_t offset, const ptrdiff_t* stride, const size_t* shape, size_t rank, size_t size, unsigned int block_size);
 
+#define DECLARE_UNARY_FLOAT_OP_HEADERS(op)                                                     \
+    void launch_##op##_contiguous_f32(float* data, size_t start, size_t len, unsigned int block_size); \
+    void launch_##op##_contiguous_f64(double* data, size_t start, size_t len, unsigned int block_size); \
+                                                                                                \
+    void launch_##op##_strided_f32(float* data, size_t offset, ptrdiff_t stride, size_t len, unsigned int block_size); \
+    void launch_##op##_strided_f64(double* data, size_t offset, ptrdiff_t stride, size_t len, unsigned int block_size); \
+                                                                                                \
+    void launch_##op##_nd_affine_f32(float* data, size_t offset, const ptrdiff_t* stride, const size_t* shape, size_t rank, size_t size, unsigned int block_size); \
+    void launch_##op##_nd_affine_f64(double* data, size_t offset, const ptrdiff_t* stride, const size_t* shape, size_t rank, size_t size, unsigned int block_size);
+
+
+DECLARE_UNARY_FLOAT_OP_HEADERS(abs)
+
 void launch_flat_contiguous_reduce_f64(const double *data, double *out, size_t start, size_t len, ReductionOpCode code, const ReductionSettings *settings, unsigned int block_size);
 void launch_nd_reduce_contiguous_f64(double *data, double *out, size_t offset, size_t outer, size_t r, size_t inner, ReductionOpCode code, const ReductionSettings *settings, unsigned int block_size);
 // void launch_elementwise_scattered_f32(float* data, const size_t* offsets, size_t n, uint8_t op, float value, unsigned int block_size);
